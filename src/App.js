dfd5dc8019e51d3c4e209e1a8de948d4572ee328
ramajinig <ramajinignanasuthan@gmail.com>
2024-12-12 17:52:44 +0530

    Ramu commit app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-// frontend/src/App.js
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Contact from './components/Contact';
-import Committee from './components/Committee';
-import theme from './Styles/Theme'
-import { ThemeProvider } from '@mui/material/styles';
-import Sidebar from './components/Themes/Sidebar';
-import Sample from './components/Themes/sample';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import RouteMain from './Routes';
 
 function App() {
-
-  return (
-    <ThemeProvider theme={theme}>
-
-<Router>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/side" element={<Sidebar />} />
-        <Route path="/sample" element={<Sample />} />
-        <Route path="/nav" element={<Navbar />} />
-
-
-      </Routes>
-    </Router>
-
-    </ThemeProvider>
-
-  );
+    return (
+        <div className="App">
+            <RouteMain />
+        </div>
+    );
 }
 
 export default App;
